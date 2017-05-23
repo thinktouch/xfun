d7f27478ff6f63db04be5209ea315a0e55d4ca76
xfun <www111981@126.com>
2017-05-23 18:47:35 +0800

    add sort method

--- src/test/java/com/xfun/demos/interview/Reorder.java
+++
@@ -1,7 +0,0 @@
-package com.xfun.demos.interview;
-
-/**
- * Created by xfun on 5/23/17.
- */
-public class Reorder {
-}
---
+++ src/test/java/com/xfun/demos/interview/ReorderTest.java
@@ -0,0 +1,33 @@
+package com.xfun.demos.interview;
+
+/**
+ * Created by xfun on 5/23/17.
+ */
+public class ReorderTest {
+
+    public static void main(String[] args){
+        stringBuilderMethod();
+        arrayMethod();
+    }
+
+    public static void stringBuilderMethod(){
+        String strBefore = "1-2-3-4-5-6--A-B-C-D";
+        System.out.println("<stringBuilderMethod>  reorder before: " + strBefore);
+
+        String strAfter = new StringBuilder(strBefore).reverse().toString();
+        System.out.println("<stringBuilderMethod>  reorder after:  " + strAfter);
+    }
+
+    public static void arrayMethod(){
+        String strBefore = "1-2-3-4-5-6--A-B-C-D";
+        System.out.println("<arrayMethod>  reorder before: " + strBefore);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = strBefore.length() - 1; i >= 0; i--){
+            stringBuilder.append(strBefore.charAt(i));
+        }
+        String strAfter = stringBuilder.toString();
+        System.out.println("<arrayMethod>  reorder after:  " + strAfter);
+    }
+
+}
